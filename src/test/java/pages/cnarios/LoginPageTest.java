de33d9fb8163e356030b9a7c516a1b0410490f72
totone-uy1990 <ronalggm@outlook.com>
2026-03-09 20:18:29 -0300

    refactor: optimize locators for stability and fix typos in LoginPage

--- src/test/java/pages/cnarios/LoginPageTest.java
+++ src/test/java/pages/cnarios/LoginPageTest.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 public class LoginPageTest extends BasePage {
     private String TestLoginFlow = "https://www.cnarios.com/challenges/login-flow";
-    private String userNameField = "(//input[contains(@class,'MuiOutlinedInput-input')])[1]";
-    private String paswordField = "(//input[contains(@class,'MuiOutlinedInput-input')])[2]";
-    private String loginButon = "button.MuiButton-containedPrimary.MuiButton-fullWidth";
-    private String messageAdmin = "(//div[contains(@class,'MuiAlert-message css-127h8j3')])[2]";
+    private String userNameField = "//label[text()='Username']/following-sibling::div//input";
+    private String passwordField = "//label[text()='Password']/following-sibling::div//input";
+    private String loginButton = "//button[text()='Login']";
+    private String messageAdmin = "//div[contains(@class, 'MuiAlert-message') and contains(., 'Success')]";
     private String DashBoardLocator = "//p[contains(text(), 'Dashboard')]";
     private String welcomeMessage = "//h5[contains(text(), 'Welcome')]";
     private String alerts = "div[role='alert']";
@@ -25,17 +25,17 @@ public class LoginPageTest extends BasePage {
         write(userNameField, words);
     }
 
-    public void writePaswordField(String words) {
-        write(paswordField, words);
+    public void writePasswordField(String words) {
+        write(passwordField, words);
 
     }
 
     public void clickButton() {
-        clickElement(loginButon);
+        clickElement(loginButton);
 
     }
 
-    //validadores
+    // validadores
     public String getAdminLoggedMessage(String locator) {
         return getTextOfWebElement(locator);
     }
@@ -58,10 +58,8 @@ public class LoginPageTest extends BasePage {
         return getWebElement(locator);
     }
 
-
     public List<WebElement> getErrorMessages() {
         return findElements(alerts);
     }
-
 
 }
--- src/test/java/steps/cnarios/loginFlowSteps.java
+++ src/test/java/steps/cnarios/loginFlowSteps.java
@@ -9,24 +9,23 @@ public class loginFlowSteps {
     final private LoginPageTest loginFlow = new LoginPageTest();
     final private CustomAssertions verify = new CustomAssertions();
 
-
     @Given("the user is on the Cnarios test_login page {string}")
     public void navigatetoLoginFlow(String url) {
         loginFlow.navigateTo(url);
     }
 
-
     @When("the user enters his credentials:")
     public void fillingUsernameField(LoginData loginData) {
 
         loginFlow.writeUsernameField(loginData.getUsername());
-        loginFlow.writePaswordField(loginData.getPassword());
+        loginFlow.writePasswordField(loginData.getPassword());
 
-        /*forma antigua:
-        LoginData data = table  .asList(LoginData.class).get(0);
-        loginFlow.writeUsernameField(data.getUsername());
-        loginFlow.writePaswordField(data.getPassword());
-    */
+        /*
+         * forma antigua:
+         * LoginData data = table .asList(LoginData.class).get(0);
+         * loginFlow.writeUsernameField(data.getUsername());
+         * loginFlow.writePaswordField(data.getPassword());
+         */
 
     }
 
@@ -35,7 +34,6 @@ public class loginFlowSteps {
         loginFlow.clickButton();
     }
 
-
     @Then("the user should see an error message saying {string}")
     public void validatingErrorMessageLogin(String message) {
         verify.assertAnyElementContainsText(loginFlow.getErrorMessages(), message);
@@ -43,46 +41,38 @@ public class loginFlowSteps {
 
     @Then("the user should be redirected to the {string} page")
     public void validatingWeAreDashBoardPanel(String expectedMessage) {
-        verify.assertContainsText
-                (loginFlow.dashBoardElement(),
-                        expectedMessage,
-                        "Mensaje de Admin");
+        verify.assertContainsText(loginFlow.dashBoardElement(),
+                expectedMessage,
+                "Mensaje de Admin");
     }
 
     @And("the user should see a welcome message containing {string}")
     public void verifyWelcomeMessage(String expectedMessage) {
-        verify.assertContainsText
-                (loginFlow.getMessageWelcome(),
-                        expectedMessage,
-                        "Mensaje de bienvenida");
+        verify.assertContainsText(loginFlow.getMessageWelcome(),
+                expectedMessage,
+                "Mensaje de bienvenida");
 
     }
-
 
     @When("the user leaves the username field empty")
     public void theUserLeavesTheUsernameFieldEmpty() {
 
     }
 
-
     @When("the user leaves the password field empty")
     public void theUserLeavesThePasswordFieldEmpty() {
     }
-
 
     @Then("the user should see a validation message {string} under the username field")
     public void theUserShouldSeeAValidationMessageUnderTheUsernameField(String message) {
 
     }
 
-
     @Then("the user should see a validation message {string} under the password field")
     public void theUserShouldSeeAValidationMessageUnderThePasswordField(String message) {
 
     }
 
-
-    //metodo adicional para campos vacios
-
+    // metodo adicional para campos vacios
 
 }
